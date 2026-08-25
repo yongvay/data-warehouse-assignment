a338e70561330400e03de99def35c51c77f3d065
Pei Qi <pqinfinity1119@gmail.com>
2026-08-26 02:09:54 +0800

    Update SQL file to reference new customer dimension

--- Task 2/Task 2b/run_task2b.sql
+++ Task 2/Task 2b/run_task2b.sql
@@ -1,7 +1,7 @@
 -- PROMPT --- COMPILING TASK 2B DIMENSIONS ---
 -- @"C:\Users\Lenovo\Desktop\Database Warehouse\data-warehouse-assignment\Task 2\Task 2b\DIM\ADDRESS_DIM_INCREMENTAL.sql"
 -- @"C:\Users\Lenovo\Desktop\Database Warehouse\data-warehouse-assignment\Task 2\Task 2b\DIM\BRANCH_DIM_INCREMENTAL.sql"
--- @"C:\Users\Lenovo\Desktop\Database Warehouse\data-warehouse-assignment\Task 2\Task 2b\DIM\CUSTOMER_DIM_INCREMENTAL.sql"
+-- @"C:\Users\Lenovo\Desktop\Database Warehouse\data-warehouse-assignment\Task 2\Task 2b\DIM\CUSTOMER_DIM_NEW_RECORD.sql"
 -- @"C:\Users\Lenovo\Desktop\Database Warehouse\data-warehouse-assignment\Task 2\Task 2b\DIM\DATE_DIM_INCREMENTAL.sql"
 -- @"C:\Users\Lenovo\Desktop\Database Warehouse\data-warehouse-assignment\Task 2\Task 2b\DIM\DELIVERY_COMPANY_DIM_INCREMENTAL.sql"
 -- @"C:\Users\Lenovo\Desktop\Database Warehouse\data-warehouse-assignment\Task 2\Task 2b\DIM\ITEM_DIM_INCREMENTAL.sql"
